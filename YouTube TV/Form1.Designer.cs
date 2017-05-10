5506026df25872d30b0b351e2a1833d75dce9046
Alee14 <alee14498@gmail.com>
2017-05-10 14:08:17 -0400

    New Project!

--- YouTube TV/Form1.Designer.cs
+++ YouTube TV/Form1.Designer.cs
@@ -28,20 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(514, 421);
+            this.panel1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(611, 452);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(511, 421);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- YouTube TV/Form1.cs
+++ YouTube TV/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using CefSharp.WinForms.Internals;
 
 namespace YouTube_TV
 {
@@ -15,6 +18,21 @@ namespace YouTube_TV
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public CefSharp.WinForms.ChromiumWebBrowser browser;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("This project was created by AleeCorp! To exit press Alt+F4. This will take some time please wait...");
+
+            browser = new CefSharp.WinForms.ChromiumWebBrowser("www.youtube.com/tv");
+            {
+                Dock = DockStyle.Fill;
+                Size = new Size(514, 421);
+                Location = new Point(0, 0);
+            }
+            this.panel1.Controls.Add(browser);
         }
     }
 }
